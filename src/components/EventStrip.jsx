bfc087e02f84a4559b32d413c621dfb9d19a4907
bigchills <yemi7164@gmail.com>
2025-02-22 01:24:38 +0100

    chore: refactor component to avoid breaks

--- src/components/EventStrip.jsx
+++ src/components/EventStrip.jsx
@@ -1,31 +1,29 @@
-import sampleThumbnail from "../assets/sample-thumbnail.png";
-import calender from "../assets/calender.svg";
-import { NavLink } from "react-router-dom";
+import sampleThumbnail from "../../public/assets/sample-thumbnail.png";
+import calender from "../../public/assets/calender.svg";
+import Link from "next/link";
 
-function EventStrip({ thumbnail, title, subTitle, date }) {
+function EventStrip({ thumbnail, title, subTitle, date, eventId }) {
   return (
     <div className="w-full py-[22px] px-[24px] bg-[#0D0C0C] flex justify-between items-center">
       <div className="flex items-stretch gap-x-[11px]">
-        <img src={sampleThumbnail} alt="" />
+        <img src={thumbnail || sampleThumbnail} alt={title} />
         <div>
-          <h3 className="mb-1 text-xl font-medium text-white">
-            Starknet Kenya Meetup
-          </h3>
+          <h3 className="mb-1 text-xl font-medium text-white">{title}</h3>
           <h5 className="text-sm text-[#C4C4C4] pb-[11px] border-b-white border-b-[0.3px]">
-            Exploring The starknet ecosystem{" "}
+            {subTitle}
           </h5>
           <h4 className="text-sm text-[#08C51A] gap-x-2 mt-2 flex items-center">
-            <img src={calender} alt="" />
-            24th March - 26th March 2024
+            <img src={calender} alt="Calendar icon" />
+            {date}
           </h4>
         </div>
       </div>
-      <NavLink
-        to="/app/event/1"
+      <Link
+        href={`/app/event/${eventId}`}
         className="py-4 px-[44px] bg-white rounded-[5px] text-sm font-medium text-[#121212]"
       >
         Go to event
-      </NavLink>
+      </Link>
     </div>
   );
 }
