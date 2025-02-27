9db8ddcbe7ea444786bd1ba76315c9743356a527
OWK50GA <okoriewifrid.20201206203@futo.edu.ng>
2025-02-27 15:28:54 +0100

    protected routes apart from the home page, with connected wallet pass

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Providers } from "../components/Providers";
 import { Toaster } from 'react-hot-toast';
 import { TanstackQueryProvider } from "@/components/provider/TanstackQueryProvider";
+import ProtectedRoute from "@/components/ProtectedRoute";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,11 @@ export default function RootLayout({ children }) {
       <body>
 
         <TanstackQueryProvider>
-          <Providers>{children}</Providers>
+          <Providers>
+            <ProtectedRoute>
+              {children}
+            </ProtectedRoute>
+          </Providers>
           <Toaster position="bottom-left" />
         </TanstackQueryProvider>
       </body>
